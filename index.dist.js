d02bf67d8697df35d7323b689338367525b7194a
Sergii Bondarenko <broken@firstvector.org>
2020-02-14 12:05:27 +0200

    Add `fast-async` instead of `regenerator-runtime`

--- index.dist.js
+++ index.dist.js
@@ -9,10 +9,6 @@ exports.getPushSubscriptionPayload = getPushSubscriptionPayload;
 exports.getPushSubscriptionFlow = getPushSubscriptionFlow;
 exports.isSupported = void 0;
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
 
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
@@ -123,198 +119,142 @@ function getPushSubscriptionFlow(sync) {
   var currentSubscription;
   return {
     getPermission: function getPermission() {
-      return _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee() {
+      return new Promise(function ($return, $error) {
         var _getLogger, debug;
 
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                _getLogger = getLogger('getPermission', logger), debug = _getLogger.debug;
-                debug(permission);
+        _getLogger = getLogger('getPermission', logger), debug = _getLogger.debug;
+        debug(permission);
 
-                if (!(permission === 'default')) {
-                  _context.next = 7;
-                  break;
-                }
-
-                _context.next = 5;
-                return Notification.requestPermission();
-
-              case 5:
-                permission = _context.sent;
-                debug(permission);
-
-              case 7:
-                return _context.abrupt("return", permission);
-
-              case 8:
-              case "end":
-                return _context.stop();
+        if (permission === 'default') {
+          return Promise.resolve(Notification.requestPermission()).then(function ($await_4) {
+            try {
+              permission = $await_4;
+              debug(permission);
+              return $If_1.call(this);
+            } catch ($boundEx) {
+              return $error($boundEx);
             }
-          }
-        }, _callee);
-      }))();
+          }.bind(this), $error);
+        }
+
+        function $If_1() {
+          return $return(permission);
+        }
+
+        return $If_1.call(this);
+      });
     },
     getSubscription: function getSubscription() {
-      return _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee2() {
+      return new Promise(function ($return, $error) {
         var _getLogger2, debug, error;
 
-        return regeneratorRuntime.wrap(function _callee2$(_context2) {
-          while (1) {
-            switch (_context2.prev = _context2.next) {
-              case 0:
-                _getLogger2 = getLogger('getSubscription', logger), debug = _getLogger2.debug, error = _getLogger2.error; // Force to call "getPermission()".
+        _getLogger2 = getLogger('getSubscription', logger), debug = _getLogger2.debug, error = _getLogger2.error;
 
-                if (!(permission !== 'granted')) {
-                  _context2.next = 4;
-                  break;
-                }
+        // Force to call "getPermission()".
+        if (permission !== 'granted') {
+          error('no permission');
+          return $error(new Error('Notifications permission is not granted.'));
+        }
 
-                error('no permission');
-                throw new Error('Notifications permission is not granted.');
+        if (currentSubscription instanceof PushSubscription) {
+          debug('reuse');
+          return $return(currentSubscription);
+        }
 
-              case 4:
-                if (!(currentSubscription instanceof PushSubscription)) {
-                  _context2.next = 7;
-                  break;
-                }
-
-                debug('reuse');
-                return _context2.abrupt("return", currentSubscription);
-
-              case 7:
-                debug('retrieve');
-                _context2.next = 10;
-                return navigator.serviceWorker.ready;
-
-              case 10:
-                _context2.next = 12;
-                return _context2.sent.pushManager.getSubscription();
-
-              case 12:
-                currentSubscription = _context2.sent;
+        debug('retrieve');
+        return Promise.resolve(navigator.serviceWorker.ready).then(function ($await_5) {
+          try {
+            return Promise.resolve($await_5.pushManager.getSubscription()).then(function ($await_6) {
+              try {
+                currentSubscription = $await_6;
 
                 if (currentSubscription instanceof PushSubscription) {
                   debug('update');
                   sync('PATCH', currentSubscription);
                 }
 
-                return _context2.abrupt("return", currentSubscription);
-
-              case 15:
-              case "end":
-                return _context2.stop();
-            }
+                return $return(currentSubscription);
+              } catch ($boundEx) {
+                return $error($boundEx);
+              }
+            }, $error);
+          } catch ($boundEx) {
+            return $error($boundEx);
           }
-        }, _callee2);
-      }))();
+        }, $error);
+      });
     },
     subscribe: function subscribe(applicationServerKey) {
-      return _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee3() {
+      return new Promise(function ($return, $error) {
         var _getLogger3, debug, error;
 
-        return regeneratorRuntime.wrap(function _callee3$(_context3) {
-          while (1) {
-            switch (_context3.prev = _context3.next) {
-              case 0:
-                _getLogger3 = getLogger('subscribe', logger), debug = _getLogger3.debug, error = _getLogger3.error; // Force to call "getSubscription()".
+        _getLogger3 = getLogger('subscribe', logger), debug = _getLogger3.debug, error = _getLogger3.error;
 
-                if (!(currentSubscription === undefined)) {
-                  _context3.next = 4;
-                  break;
+        // Force to call "getSubscription()".
+        if (currentSubscription === undefined) {
+          error('not checked');
+          return $error(new Error('Check for the existing subscription before requesting a new one.'));
+        }
+
+        if (currentSubscription === null) {
+          debug('request');
+          return Promise.resolve(navigator.serviceWorker.ready).then(function ($await_7) {
+            try {
+              return Promise.resolve($await_7.pushManager.subscribe({
+                applicationServerKey: decodeKey(applicationServerKey),
+                userVisibleOnly: true
+              })).then(function ($await_8) {
+                try {
+                  currentSubscription = $await_8;
+                  debug('create');
+                  sync('POST', currentSubscription);
+                  return $If_2.call(this);
+                } catch ($boundEx) {
+                  return $error($boundEx);
                 }
-
-                error('not checked');
-                throw new Error('Check for the existing subscription before requesting a new one.');
-
-              case 4:
-                if (!(currentSubscription === null)) {
-                  _context3.next = 14;
-                  break;
-                }
-
-                debug('request');
-                _context3.next = 8;
-                return navigator.serviceWorker.ready;
-
-              case 8:
-                _context3.t0 = {
-                  applicationServerKey: decodeKey(applicationServerKey),
-                  userVisibleOnly: true
-                };
-                _context3.next = 11;
-                return _context3.sent.pushManager.subscribe(_context3.t0);
-
-              case 11:
-                currentSubscription = _context3.sent;
-                debug('create');
-                sync('POST', currentSubscription);
-
-              case 14:
-                return _context3.abrupt("return", currentSubscription);
-
-              case 15:
-              case "end":
-                return _context3.stop();
+              }.bind(this), $error);
+            } catch ($boundEx) {
+              return $error($boundEx);
             }
-          }
-        }, _callee3);
-      }))();
+          }.bind(this), $error);
+        }
+
+        function $If_2() {
+          return $return(currentSubscription);
+        }
+
+        return $If_2.call(this);
+      });
     },
     unsubscribe: function unsubscribe() {
-      return _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee4() {
+      return new Promise(function ($return, $error) {
         var _getLogger4, debug, error;
 
-        return regeneratorRuntime.wrap(function _callee4$(_context4) {
-          while (1) {
-            switch (_context4.prev = _context4.next) {
-              case 0:
-                _getLogger4 = getLogger('unsubscribe', logger), debug = _getLogger4.debug, error = _getLogger4.error; // Force to call "subscribe()" or "getSubscription()".
+        _getLogger4 = getLogger('unsubscribe', logger), debug = _getLogger4.debug, error = _getLogger4.error;
 
-                if (!(currentSubscription instanceof PushSubscription)) {
-                  _context4.next = 12;
-                  break;
-                }
-
-                debug('request');
-                _context4.next = 5;
-                return currentSubscription.unsubscribe();
-
-              case 5:
-                if (!_context4.sent) {
-                  _context4.next = 10;
-                  break;
-                }
-
+        // Force to call "subscribe()" or "getSubscription()".
+        if (currentSubscription instanceof PushSubscription) {
+          debug('request');
+          return Promise.resolve(currentSubscription.unsubscribe()).then(function ($await_9) {
+            try {
+              if ($await_9) {
                 debug('delete');
                 sync('DELETE', currentSubscription);
                 currentSubscription = null;
-                return _context4.abrupt("return", null);
+                return $return(null);
+              }
 
-              case 10:
-                error('failed');
-                throw new Error('Unable to unsubscribe.');
-
-              case 12:
-                error('logic violation');
-                throw new Error('Unsubscription cannot be performed while there is no subscription.');
-
-              case 14:
-              case "end":
-                return _context4.stop();
+              error('failed');
+              return $error(new Error('Unable to unsubscribe.'));
+            } catch ($boundEx) {
+              return $error($boundEx);
             }
-          }
-        }, _callee4);
-      }))();
+          }, $error);
+        }
+
+        error('logic violation');
+        return $error(new Error('Unsubscription cannot be performed while there is no subscription.'));
+      });
     }
   };
 }
